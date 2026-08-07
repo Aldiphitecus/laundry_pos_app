9ac19909777c2b906af502b57398228ab5cf5d13
Aldi <nurfahmialdi123@gmail.com>
2026-08-07 21:33:37 +0700

    feat: initial commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:laundry_pos_app/screens/home/home_screen.dart';
+
+class LaundryApp extends StatelessWidget {
+  const LaundryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:laundry_pos_app/app.dart';
+
+void main() {
+  runApp(const LaundryApp());
+}
