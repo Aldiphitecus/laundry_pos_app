d71be25e80ee7b98d16ad753cad9481d0e268746
Aldi <nurfahmialdi123@gmail.com>
2026-08-14 15:23:19 +0700

    feat: add chart

--- lib/screens/home/widgets/chart_widget.dart
+++ lib/screens/home/widgets/chart_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:laundry_pos_app/core/constants/app_colors.dart';
 
 class ChartWidget extends StatelessWidget {
   const ChartWidget({super.key});
@@ -11,7 +13,7 @@ class ChartWidget extends StatelessWidget {
         side: BorderSide(color: Colors.grey.withValues(alpha: 0.2)),
       ),
       child: Padding(
-        padding: EdgeInsets.all(14.0),
+        padding: const EdgeInsets.all(14.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -20,9 +22,74 @@ class ChartWidget extends StatelessWidget {
               style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.w600),
             ),
             const SizedBox(height: 9.0),
+            SizedBox(
+              height: 200,
+              child: BarChart(
+                BarChartData(
+                  alignment: BarChartAlignment.spaceEvenly,
+                  maxY: 500000,
+                  titlesData: FlTitlesData(
+                    leftTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    topTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    rightTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: (value, meta) {
+                          const days = [
+                            'Sen',
+                            'Sel',
+                            'Rab',
+                            'Kam',
+                            'Jum',
+                            'Sab',
+                            'Min',
+                          ];
+                          return Text(
+                            days[value.toInt()],
+                            style: const TextStyle(fontSize: 11),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  gridData: const FlGridData(show: false),
+                  borderData: FlBorderData(show: false),
+                  barGroups: [
+                    _buildBar(0, 250000),
+                    _buildBar(1, 320000),
+                    _buildBar(2, 180000),
+                    _buildBar(3, 400000),
+                    _buildBar(4, 280000),
+                    _buildBar(5, 450000),
+                    _buildBar(6, 150000),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  BarChartGroupData _buildBar(int x, double y) {
+    return BarChartGroupData(
+      x: x,
+      barRods: [
+        BarChartRodData(
+          toY: y,
+          color: AppColors.tertiary,
+          width: 30,
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ],
     );
   }
 }
--- lib/screens/home/widgets/total_transactions_widget.dart
+++ lib/screens/home/widgets/total_transactions_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laundry_pos_app/core/constants/app_colors.dart';
 
 class TotalTransactionsWidget extends StatelessWidget {
   const TotalTransactionsWidget({super.key});
@@ -22,7 +23,11 @@ class TotalTransactionsWidget extends StatelessWidget {
             const SizedBox(height: 7.0),
             const Text(
               '142',
-              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                color: AppColors.secondary,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
