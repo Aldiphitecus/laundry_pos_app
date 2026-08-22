700b42c970cc7d1e868dacea1304b8dc62cd6f16
Aldi <nurfahmialdi123@gmail.com>
2026-08-22 12:21:44 +0700

    feat: add button to create new transaction

--- lib/screens/home/create_transaction/create_transaction.dart
+++ lib/screens/home/create_transaction/create_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laundry_pos_app/core/constants/app_colors.dart';
 import 'package:laundry_pos_app/core/widgets/main_app_bar.dart';
 
 class CreateTransaction extends StatelessWidget {
@@ -9,6 +10,17 @@ class CreateTransaction extends StatelessWidget {
     return Scaffold(
       appBar: MainAppBar(title: 'Transaksi Baru'),
       body: Center(child: const Text('Halaman untuk membuat transaksi baru')),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          print('You clicked');
+        },
+        label: const Text(
+          'Pilih Layanan',
+          style: const TextStyle(color: Colors.white),
+        ),
+        icon: Icon(Icons.add, color: Colors.white),
+        backgroundColor: AppColors.primary,
+      ),
     );
   }
 }
