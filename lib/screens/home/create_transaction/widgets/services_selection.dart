612bdf65d5b01043abd38cd73037e4ef2c342637
Aldi <nurfahmialdi123@gmail.com>
2026-08-24 16:01:42 +0700

    feat: add service selection modal

--- lib/screens/home/create_transaction/widgets/services_selection.dart
+++ lib/screens/home/create_transaction/widgets/services_selection.dart
@@ -5,6 +5,36 @@ class ServicesSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Pilihan layanan')));
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 20,
+        top: 10,
+        right: 20,
+        bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 60,
+              height: 6,
+              decoration: BoxDecoration(
+                color: Colors.grey[400],
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Pilih Layanan',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
   }
 }
