e087ce8b4d176350e2f199f8e695b64a27aa0d3c
Aldi <nurfahmialdi123@gmail.com>
2026-09-21 14:50:07 +0700

    feat: add transaction model

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -1,3 +1,5 @@
+import 'package:laundry_pos_app/models/transaction_item_model.dart';
+
 class TransactionModel {
   final String id;
   final String transactionNumber;
@@ -16,22 +18,30 @@ class TransactionModel {
     required this.createdAt,
     required this.items,
   });
-}
 
-class TransactionItemModel {
-  final String serviceId;
-  final String serviceName;
-  final double qty;
-  final String unit;
-  final int price;
-  final int total;
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'transaction_number': transactionNumber,
+      'customer_name': customerName,
+      'transaction_date': transactionDate.toIso8601String(),
+      'total': total,
+      'create_at': createdAt.toIso8601String(),
+    };
+  }
 
-  const TransactionItemModel({
-    required this.serviceId,
-    required this.serviceName,
-    required this.qty,
-    required this.unit,
-    required this.price,
-    required this.total,
-  });
+  factory TransactionModel.fromMap(
+    Map<String, dynamic> map, {
+    List<TransactionItemModel> items = const [],
+  }) {
+    return TransactionModel(
+      id: map['id'] as String,
+      transactionNumber: map['transaction_number'] as String,
+      customerName: map['customer_name'] as String,
+      transactionDate: DateTime.parse(map['transaction_date'] as String),
+      total: map['total'] as int,
+      createdAt: DateTime.parse(map['created_at'] as String),
+      items: items,
+    );
+  }
 }
